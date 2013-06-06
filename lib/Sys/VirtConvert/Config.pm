razorinc/virt-v2v

commit 9817c1968e1bd60a0c05626b00eb8611a1236c66

Eliminate $desc from the conversion process
$desc was a hangover from before inspection code moved into libguestfs. This
patch finally eliminates it in favour of direct calls to libguestfs inspection
code.

Combine some almost duplicate code: _init_kernels and _list_kernels in the
RedHat module.

lib/Sys/VirtConvert/Config.pm
@@ -44,7 +44,7 @@ Sys::VirtConvert::Config - Manage virt-v2v's configuration file
  $eh = Sys::VirtConvert::Config->new(@config_paths);
 
  my $isopath = $config->get_transfer_iso();
- my ($path, $deps) = $config->match_app($desc, $name, $arch);
+ my ($path, $deps) = $config->match_app($g, $root, $name, $arch);
  my ($name, $type) = $config->map_network($oldname, $oldtype);
 
 =head1 DESCRIPTION
@@ -278,12 +278,12 @@ sub unmount_transfer
 
 sub _get_search
 {
-    my ($desc, $name, $arch) = @_;
+    my ($g, $root, $name, $arch) = @_;
 
-    my $os     = $desc->{os};
-    my $distro = $desc->{distro};
-    my $major  = $desc->{major_version};
-    my $minor  = $desc->{minor_version};
+    my $os     = $g->inspect_get_type($root);
+    my $distro = $g->inspect_get_distro($root);
+    my $major  = $g->inspect_get_major_version($root);
+    my $minor  = $g->inspect_get_minor_version($root);
 
     my $search = "os='$os'";
     $search .= " name='$name'";
@@ -307,9 +307,9 @@ the app's listed dependencies.
 sub match_app
 {
     my $self = shift;
-    my ($desc, $name, $arch) = @_;
+    my ($g, $root, $name, $arch) = @_;
 
-    my $app = $self->_match_element('app', $desc, $name, $arch);
+    my $app = $self->_match_element('app', $g, $root, $name, $arch);
 
     my %app;
     my ($path) = $app->findnodes('path/text()');
@@ -360,9 +360,9 @@ Returns undef if the capability was not found.
 sub match_capability
 {
     my $self = shift;
-    my ($desc, $name, $arch) = @_;
+    my ($g, $root, $name, $arch) = @_;
 
-    my $cap = $self->_match_element('capability', $desc, $name, $arch);
+    my $cap = $self->_match_element('capability', $g, $root, $name, $arch);
 
     my %out;
     foreach my $dep ($cap->findnodes('dep')) {
@@ -398,29 +398,29 @@ sub match_capability
 sub _match_element
 {
     my $self = shift;
-    my ($type, $desc, $name, $arch) = @_;
+    my ($type, $g, $root, $name, $arch) = @_;
 
     v2vdie __x('No config specified. No {type} match for {search}.',
-               type => $type, search => _get_search($desc, $name, $arch))
+               type => $type, search => _get_search($g, $root, $name, $arch))
         if @{$self->{doms}} == 0;
 
     foreach my $dom (@{$self->{doms}}) {
-        my $match = _match_element_dom($dom, $type, $desc, $name, $arch);
+        my $match = _match_element_dom($dom, $type, $g, $root, $name, $arch);
         return $match if defined($match);
     }
 
     v2vdie __x('No {type} in config matches {search}',
-               type => $type, search => _get_search($desc, $name, $arch));
+               type => $type, search => _get_search($g, $root, $name, $arch));
 }
 
 sub _match_element_dom
 {
-    my ($dom, $type, $desc, $name, $arch) = @_;
+    my ($dom, $type, $g, $root, $name, $arch) = @_;
 
-    my $os     = $desc->{os};
-    my $distro = $desc->{distro};
-    my $major  = $desc->{major_version};
-    my $minor  = $desc->{minor_version};
+    my $os     = $g->inspect_get_type($root);
+    my $distro = $g->inspect_get_distro($root);
+    my $major  = $g->inspect_get_major_version($root);
+    my $minor  = $g->inspect_get_minor_version($root);
 
     # Check we've got at least the {os} field from OS detection.
     v2vdie __('Didn\'t detect operating system') unless defined $os;

lib/Sys/VirtConvert/Converter.pm
@@ -102,23 +102,10 @@ sub convert
 
     $config->mount_transfer($g);
 
-    # Construct the "$desc" hashref which contains the main features
-    # found by inspection.
-    my %desc;
-
-    $desc{os}               = $g->inspect_get_type($root);
-    $desc{distro}           = $g->inspect_get_distro($root)
-        if $desc{os} eq 'linux';
-    $desc{product_name}     = $g->inspect_get_product_name($root);
-    $desc{major_version}    = $g->inspect_get_major_version($root);
-    $desc{minor_version}    = $g->inspect_get_minor_version($root);
-    $desc{arch}             = $g->inspect_get_arch($root);
-
     # Find a module which can convert the guest and run it
     foreach my $module ($class->modules()) {
-        if($module->can_handle(\%desc)) {
-            $guestcaps = $module->convert($g, $root, $config, \%desc, $meta,
-                                          $options);
+        if($module->can_handle($g, $root)) {
+            $guestcaps = $module->convert($g, $root, $config, $meta, $options);
             last;
         }
     }

lib/Sys/VirtConvert/Converter/Windows.pm
@@ -45,7 +45,7 @@ Sys::VirtConvert::Converter::Windows - Pre-convert a Windows guest to run on KVM
 
  use Sys::VirtConvert::Converter;
 
- Sys::VirtConvert::Converter->convert($g, $config, $desc, $meta);
+ Sys::VirtConvert::Converter->convert($g, $root, $config, $meta, $options);
 
 =head1 DESCRIPTION
 
@@ -62,23 +62,21 @@ on KVM.
 
 =over
 
-=item Sys::VirtConvert::Converter::Windows->can_handle(desc)
+=item Sys::VirtConvert::Converter::Windows->can_handle(g, root)
 
-Return 1 if Sys::VirtConvert::Converter::Windows can convert the guest
-described by I<desc>, 0 otherwise.
+Return 1 if Sys::VirtConvert::Converter::Windows can convert the given guest.
 
 =cut
 
 sub can_handle
 {
     my $class = shift;
+    my ($g, $root) = @_;
 
-    my $desc = shift;
-
-    return ($desc->{os} eq 'windows');
+    return ($g->inspect_get_type($root) eq 'windows');
 }
 
-=item Sys::VirtConvert::Converter::Windows->convert(g, root, config, desc, meta)
+=item Sys::VirtConvert::Converter::Windows->convert(g, root, config, meta, options)
 
 (Pre-)convert a Windows guest. Assume that can_handle has previously
 returned 1.
@@ -97,13 +95,13 @@ The root device of this operating system.
 
 An initialised Sys::VirtConvert::Config object.
 
-=item desc
-
-A description of the guest OS (see Sys::VirtConvert::Converter->convert).
-
 =item meta
 
-Guest metadata.
+Not used by the Windows converter
+
+=item options
+
+Not used by the Windows converter
 
 =back
 
@@ -113,13 +111,13 @@ sub convert
 {
     my $class = shift;
 
-    my ($g, $root, $config, $desc, undef) = @_;
+    my ($g, $root, $config, undef, undef) = @_;
 
     my $tmpdir = tempdir (CLEANUP => 1);
 
     # Note: disks are already mounted by main virt-v2v script.
 
-    my $firstboot = _upload_files ($g, $tmpdir, $desc, $config);
+    my $firstboot = _upload_files ($g, $root, $tmpdir, $config);
 
     # Open the system and software hives
     my ($sys_guest, $sys_local) = _download_hive($g, $tmpdir, 'system');
@@ -141,8 +139,8 @@ sub convert
     _disable_processor_drivers($h_sys, $current_cs);
 
     my ($block, $net) =
-        _prepare_virtio_drivers($g, $desc, $config, $h_soft);
-    _add_viostor_to_registry($desc, $h_sys, $current_cs) if $block eq 'virtio';
+        _prepare_virtio_drivers($g, $root, $config, $h_soft);
+    _add_viostor_to_registry($g, $root, $h_sys, $current_cs) if $block eq 'virtio';
 
     # Commit and upload the modified registry hives
     $h_sys->commit(undef); undef $h_sys;
@@ -158,7 +156,7 @@ sub convert
 
     $guestcaps{block} = $block;
     $guestcaps{net}   = $net;
-    $guestcaps{arch}  = $desc->{arch};
+    $guestcaps{arch}  = $g->inspect_get_arch($root);
     $guestcaps{acpi}  = 1; # XXX
 
     # We want an i686 guest for i[345]86
@@ -194,9 +192,7 @@ sub _download_hive
 # See http://rwmj.wordpress.com/2010/04/30/tip-install-a-device-driver-in-a-windows-vm/
 sub _add_viostor_to_registry
 {
-    my $desc = shift;
-    my $h = shift;
-    my $current_cs = shift;
+    my ($g, $root, $h, $current_cs) = @_;
 
     # Make the changes.
     my $regedits = <<REGEDITS;
@@ -246,13 +242,14 @@ sub _add_viostor_to_registry
 REGEDITS
 
     my $io;
-    if ($desc->{major_version} == 5 || $desc->{major_version} == 6) {
+    my $major_version = $g->inspect_get_major_version($root);
+    if ($major_version == 5 || $major_version == 6) {
         $io = IO::String->new ($regedits);
     } else {
         v2vdie __x('Guest is not a supported version of Windows '.
                    '({major}.{minor})',
-                   major => $desc->{major_version},
-                   minor => $desc->{minor_version});
+                   major => $major_version,
+                   minor => $g->inspect_get_minor_version($root));
     }
 
     local *_map = sub {
@@ -305,10 +302,7 @@ REGEDITS
 # be searched automatically when automatically installing drivers.
 sub _prepare_virtio_drivers
 {
-    my $g = shift;
-    my $desc = shift;
-    my $config = shift;
-    my $h = shift;
+    my ($g, $root, $config, $h) = @_;
 
     # Copy the target VirtIO drivers to the guest
     my $driverdir = File::Spec->catdir($g->case_sensitive_path("/windows"), "Drivers/VirtIO");
@@ -318,7 +312,7 @@ sub _prepare_virtio_drivers
     # Check for a virtio entry in the config file for this OS
     my $virtio;
     eval {
-        ($virtio) = $config->match_app ($desc, 'virtio', $desc->{arch});
+        ($virtio) = $config->match_app($g, $root, 'virtio', $g->inspect_get_arch($root));
     };
     if ($@) {
         my $block = 'ide';
@@ -416,13 +410,10 @@ sub _prepare_virtio_drivers
 # Returns 1 if RHEV APT is available, 0 otherwise.
 sub _upload_files
 {
-    my $g = shift;
-    my $tmpdir = shift;
-    my $desc = shift;
-    my $config = shift;
+    my ($g, $root, $tmpdir, $config) = @_;
 
     # Check we have virtio
-    my ($v_path) = $config->match_app ($desc, 'virtio', $desc->{arch});
+    my ($v_path) = $config->match_app($g, $root, 'virtio', $g->inspect_get_arch($root));
     my $v_local = $config->get_transfer_path($v_path);
 
     # We can't proceed if there are any files missing
@@ -440,7 +431,7 @@ sub _upload_files
     my @fb_missing;
     my @fb_files;
     for my $file ("firstboot", "firstbootapp", "rhsrvany") {
-        my ($path) = $config->match_app ($desc, $file, $desc->{arch});
+        my ($path) = $config->match_app($g, $root, $file, $g->inspect_get_arch($root));
         my $local = $config->get_transfer_path($path);
 
         if (defined($local) && $g->exists($local)) {

rwmjones/old-virt-v2v

commit d626be125eb659320273d8804e3e4318fcbb5ed3

windows: Fix matching apps for Windows guests
This fixes a regression introduced by 9817c1968e1bd60a0c05626b00eb8611a1236c66.
This change caused v2v to try to match a distro for Windows guests, causing the
lookup to fail.

lib/Sys/VirtConvert/Config.pm
@@ -281,7 +281,7 @@ sub _get_search
     my ($g, $root, $name, $arch) = @_;
 
     my $os     = $g->inspect_get_type($root);
-    my $distro = $g->inspect_get_distro($root);
+    my $distro = $g->inspect_get_distro($root) if $os eq 'linux';
     my $major  = $g->inspect_get_major_version($root);
     my $minor  = $g->inspect_get_minor_version($root);
 
@@ -418,7 +418,7 @@ sub _match_element_dom
     my ($dom, $type, $g, $root, $name, $arch) = @_;
 
     my $os     = $g->inspect_get_type($root);
-    my $distro = $g->inspect_get_distro($root);
+    my $distro = $g->inspect_get_distro($root) if $os eq 'linux';
     my $major  = $g->inspect_get_major_version($root);
     my $minor  = $g->inspect_get_minor_version($root);
 